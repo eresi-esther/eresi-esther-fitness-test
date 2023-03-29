8bbbd1eedfca0015b248576bd0476e6039306887
Esther <estherchidinma91@gmail.com>
2023-03-29 20:58:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const change1 = document.getElementById('image1');
+
+change1.addEventListener('click', (event) =>{
+    event.target.style.background = 'red'
+    event.target.style.color = 'white'
+})
+
+const change2 = document.getElementById('image2');
+
+change2.addEventListener('click', (event) =>{
+    event.target.style.background = 'green'
+    event.target.style.color = 'white'
+})
+
+const change3 = document.getElementById('image3');
+
+change3.addEventListener('click', (event) =>{
+    event.target.style.background = 'yellow'
+    event.target.style.color = 'red'
+})
+
+const change4 = document.getElementById('image4');
+
+change4.addEventListener('click', (event) =>{
+    event.target.style.background = 'orange'
+    event.target.style.color = 'white'
+})
+
+const change5 = document.getElementById('image5');
+
+change5.addEventListener('click', (event) =>{
+    event.target.style.background = 'pink'
+    event.target.style.color = 'purple'
+})
+
+const change6 = document.getElementById('image6');
+
+change6.addEventListener('click', (event) =>{
+    event.target.style.background = 'black'
+    event.target.style.color = 'white'
+})
+
+const change7 = document.getElementById('image7');
+
+change7.addEventListener('click', (event) =>{
+    event.target.style.background = 'blue'
+    event.target.style.color = 'red'
+})
+
+const change8 = document.getElementById('image8');
+
+change8.addEventListener('click', (event) =>{
+    event.target.style.background = 'green'
+    event.target.style.color = 'white'
+})
